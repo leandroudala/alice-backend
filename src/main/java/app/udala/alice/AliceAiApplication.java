3e78668016bca3d2079a2a9a31f58457bc73915d
Leandro Udala <leandro.correia@radixeng.com.br>
2025-05-04 17:46:29 -0300

    chore: created spring boot template

---
+++ src/main/java/app/udala/alice/AliceAiApplication.java
@@ -0,0 +1,13 @@
+package app.udala.alice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliceAiApplication.class, args);
+	}
+
+}
