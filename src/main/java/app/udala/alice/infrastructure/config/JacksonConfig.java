a83e3c3e9b71a228df7e673d4690429897e15f9d
Leandro Udala <leandro.correia@radixeng.com.br>
2025-05-04 21:54:46 -0300

    chore: adding configuration to remove nulls from response

---
+++ src/main/java/app/udala/alice/infrastructure/config/JacksonConfig.java
@@ -0,0 +1,17 @@
+package app.udala.alice.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper;
+    }
+}
